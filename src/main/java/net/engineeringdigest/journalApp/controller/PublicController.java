5fa68b850f402901c446cfe073948103e7714087
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2025-07-23 23:04:26 +0500

    signup and login with help of authentication manager and Jwt token

--- src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -1,28 +1,63 @@
 package net.engineeringdigest.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.service.UserDetailsServiceImpl;
 import net.engineeringdigest.journalApp.service.UserService;
+import net.engineeringdigest.journalApp.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+
 
     @GetMapping("/healthcheck")
     public String getHealth(){
         return "OK";
     }
 
-    @PostMapping("/create-user")
-    public ResponseEntity<User> createUser(@RequestBody User newUser) {
+    @PostMapping("/sign-up")
+    public ResponseEntity<User> signup(@RequestBody User newUser) {
 
         userService.saveNewUser(newUser);
         return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return  new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Error while logging in ", e);
+           return new ResponseEntity<>("Incorrect username or password ", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+
 }
